cf0524af95dc15a0e5803495703df128f268cb2d
HDuck <1ntranc3d@gmail.com>
2018-11-04 20:00:42 +0300

    Styled, positioned, added motion to sidebar blocks

---
+++ src/sub-menu/sub-menu.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('.sub-menu').accordion({
+        active: false,
+        collapsible: true,
+        heightStyle: 'auto',
+        beforeActivate: function(evt, ui) {
+            ui.newHeader.height(47).css({
+                padding: '19px 20px'
+            });
+            ui.oldHeader.height(67).css({
+                padding: '27px 20px 19px'
+            });
+        }
+    });
+});
